b82879b84b42af059565b8d2a54b48b85af4fa69
Arka-cell <samirahmane.trading@gmail.com>
2023-10-01 23:34:46 +0100

    feat: add function to retrieve random debit cards

---
+++ utils/helpers.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+
+const getRandomMasterCards = async () => {
+    const randomCardApiUrl = 'https://random-data-api.com/api/v2/credit_cards?size=50'
+    const response = await axios.get(randomCardApiUrl)
+    // eslint-disable-next-line array-callback-return
+    const data = response.data.filter(item => {
+        if (item.credit_card_type === 'mastercard') {
+            const creditCardDigits = item.credit_card_number
+            const lastDigits = creditCardDigits.slice(-4)
+            const anonymizedCCNumber = '****-****-****-' + lastDigits
+            item.credit_card_number = anonymizedCCNumber
+            return {
+                ...item
+            }
+        }
+    })
+    if (data.length === 0) {
+        /* NOTE: due to randomness, we're handling a probability case where
+        type mastercard isn't fetched with a recursive function */
+        getRandomMasterCards()
+    }
+    return data
+}
+
+module.exports = { getRandomMasterCards }
